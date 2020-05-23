71b6547a84a66c4883300849c3acc6fbe7f4fce0
sidd023 <48944821+sidd023@users.noreply.github.com>
2020-05-23 16:19:08 +1000

    update index.js

--- cloud_A2/src/index.js
+++ cloud_A2/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bulma/css/bulma.min.css';
-import './index.css';
 import App from './App';
 import Amplify from "aws-amplify";
 import config from "./config";
